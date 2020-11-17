63870a4d4fd0b45b151acbaa2c4e1e81cebba1ec
Jump3rr <theneonxd@gmail.com>
2020-11-17 02:13:49 +0100

    stworzenie szkieletu projektu, stworzenie enum UnitOfMeasure, wrzucenie testów

---
+++ PudelkoLib/Pudelko.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PudelkoLib
+{
+    public sealed class Pudelko : IFormattable, IEquatable<Pudelko>, IEnumerable
+    {
+
+    }
+
+    public enum UnitOfMeasure : ushort
+    {
+        milimeter = 1000,
+        centimeter = 100,
+        meter = 1
+    }
+}
