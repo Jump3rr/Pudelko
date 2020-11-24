41b11bc8643d4ed4634c845e84d3875b0d009b53
Jump3rr <theneonxd@gmail.com>
2020-11-24 21:38:53 +0100

    dodano Kompresuj

---
+++ PudelkoApp/KompresujP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PudelkoLib;
+
+namespace PudelkoApp
+{
+    public static class KompresujP
+    {
+        public static Pudelko Kompresuj(this Pudelko p)
+        {
+            double a = Math.Cbrt(p.Objetosc);
+            return new Pudelko(a, a, a);
+        }
+        
+    }
+}
